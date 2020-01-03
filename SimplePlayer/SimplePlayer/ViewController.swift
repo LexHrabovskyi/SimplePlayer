82f52ebbb6142c4a6a77fe34d335e4794e552986
Lex <lex.hrabovskyi@gmail.com>
2020-01-03 20:34:27 +0600

    created project

---
+++ SimplePlayer/SimplePlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimplePlayer
+//
+//  Created by Александр on 03.01.2020.
+//  Copyright © 2020 Александр. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
